133f36db0e3823ebdc5c516a88acfa1becd12813
lastkarrde <last.karrde@gmail.com>
2011-08-21 10:56:21 +1200

    wtf is going on

--- basic.py
+++ basic.py
@@ -2,27 +2,30 @@ import sys
 
 from PySide import QtCore, QtGui, QtWebKit
 
+from PySide.QtCore import QUrl
+from PySide.QtGui import QApplication, QGridLayout, QWidget
+from PySide.QtWebKit import QWebView
 
-class Basic(QtGui.QWidget):
+class Basic(QWidget):
 
     def __init__(self, parent=None):
         super(Basic, self).__init__(parent)
 
         self.setWindowTitle('Basic Application')
 
-        url = QtCore.QUrl('http://www.bitcoin.org/')
+        url = QUrl('http://www.bitcoin.org/')
 
-        self.webView = QtWebKit.QWebView()
+        self.webView = QWebView()
         self.webView.setUrl(url)
 
-        self.mainLayout = QtGui.QGridLayout()
+        self.mainLayout = QGridLayout()
         self.mainLayout.addWidget(self.webView)
 
         self.setLayout(self.mainLayout)
 
 if __name__ == '__main__':
     
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
     basic = Basic()
     basic.show()

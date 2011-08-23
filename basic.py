a7b100d6f02b2fdd567f82b6c6c9c1f9ffc0852a
lastkarrde <last.karrde@gmail.com>
2011-08-23 23:16:08 +1200

    More comments for the most basic example

--- basic.py
+++ basic.py
@@ -7,27 +7,34 @@ from PySide.QtCore import QUrl
 from PySide.QtGui import QApplication, QGridLayout, QWidget
 from PySide.QtWebKit import QWebView
 
+
+# declare our class which inherits from QWidget (this essentially makes it a window)
 class Basic(QWidget):
 
     def __init__(self, parent=None):
+        # call QWidget's __init__
         super(Basic, self).__init__(parent)
 
         self.setWindowTitle('Basic Application')
 
         url = QUrl('http://www.bitcoin.org/')
 
+        # declare our QWebView and set the URL. The URL must be an instance of QUrl, not a string
         self.webView = QWebView()
         self.webView.setUrl(url)
 
+        # set up our layout and add the instance of the QWebView
         self.mainLayout = QGridLayout()
         self.mainLayout.addWidget(self.webView)
 
         self.setLayout(self.mainLayout)
 
+# if our program is being called directly..
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
 
+    # instantiate and draw our window
     basic = Basic()
     basic.show()
     
